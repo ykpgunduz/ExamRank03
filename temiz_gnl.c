3e8a8efcb4601a812365b4045132d0229fa6a8f9
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-07-28 00:36:51 +0300

    fix

--- temiz_gnl.c
+++ temiz_gnl.c
@@ -35,13 +35,13 @@ int str_append_mem(char **s1, char *s2, size_t size2)
     size_t size1 = ft_strlen(*s1);
     char *tmp = malloc(size2 + size1 +1);
     if (!tmp)
-        return 0;
+        return (0);
     ft_memcpy(tmp, *s1, size1);
     ft_memcpy(tmp + size1, s2, size2);
     tmp[size1 + size2] = '\0';
     free(*s1);
     *s1 = tmp;
-    return 1;
+    return (1);
 }
 
 int str_append_str(char **s1, char *s2)
